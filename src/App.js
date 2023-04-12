c814833c913ebb307cb19f45da26509a5dd58b1c
Artur  Feldens <artur.feldens@leadforce.com.br>
2023-04-12 17:42:07 -0300

    implementado componentes de classe com state, arrow functions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    name: 'Artur Feldens',
+    counter: 0
+  };
+
+    //  não precisamos utilizar o bind pois o arrow function supre isso
+    // this.handlePClick = this.handlePClick.bind(this);
+
+
+  handlePClick = () => {
+    this.setState({ name: 'Feldens'})
+  }
+
+  // arrow function herdando state do constructor
+  handleAClick = (event) => {
+    // impede o elemento de realizar seu evento padrão, neste caso é abrir a url em nova pagina
+    event.preventDefault();
+    const { counter } = this.state;
+    this.setState({ counter: counter + 1})
+  }
+
+  render() {
+    const {name, counter} = this.state;
+    
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p onClick={this.handlePClick}>
+            {name} {counter}
+          </p>
+          <a
+            onClick={this.handleAClick}
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Este é o link
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
+
 
 export default App;
